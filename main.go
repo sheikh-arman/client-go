58e55571eb328f0101bbf1ef0af2a69d5fbbe2a8
sheikh-arman <arman.cse4.bu@gmail.com>
2023-04-18 12:51:59 +0600

    Api Server 002

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"Api-Server/handler"
 	"Api-Server/platform/newsfeed"
-	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi"
 	"net/http"
@@ -20,18 +19,8 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/newsfeed", handler.NewsfeedGet(feed))
+	r.Post("/newsfeed", handler.NewsfeedPost(feed))
 
-	r.Post("/newsfeed", func(w http.ResponseWriter, r *http.Request) {
-		request := map[string]string{}
-		json.NewDecoder(r.Body).Decode(request)
-
-		feed.Add(newsfeed.Item{
-			Title: request["title"],
-			Post:  request["post"],
-		})
-
-		w.Write([]byte("Good job!"))
-	})
 	fmt.Println("Serving on " + port)
 	http.ListenAndServe(port, r)
 
